6897bf8cd5bca83f3e53c2ca9b402a8dd21ee7a9
Yurik <yurik522@gmail.com>
2022-02-25 18:48:10 +0300

    Example005_Condition_if_else

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "светлана")
+{
+    Console.WriteLine("Ура, это же моя любимая!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
