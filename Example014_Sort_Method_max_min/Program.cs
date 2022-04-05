b1b4c809d2d93f20207cdbea862e34a9a47d8f2c
Yurik <yurik522@gmail.com>
2022-04-05 21:07:51 +0300

    Example014_Sort_Method_max_min

---
+++ Example014_Sort_Method_max_min/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] arr = {1, 4, 3, 8, 2, 4, 7, 9, 11, 3, 2, 1};
+void PrintArray(int[] array) 
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();    
+}
+void selectionSort(int[] array) 
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+         int maxPosition = i; 
+        for(int j = i + 1; j < array.Length; j++ )
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;                            
+    }
+}
+PrintArray(arr);
+selectionSort(arr);
+Console.WriteLine();
+PrintArray(arr);
