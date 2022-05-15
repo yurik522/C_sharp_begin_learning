9bc73594883dc70f272853b5e14ea0c328f1c724
Yurik <yurik522@gmail.com>
2022-05-15 11:21:39 +0300

    Example017 and Example018

---
+++ Example017_Factorial_Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿// double Factorial(int n)
+// {
+//     if (n == 1) return n;
+//     else return n * Factorial(n-1);
+// }
+// //Console.WriteLine(Factorial(25));
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return n;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 46; i++)
+{
+    Console.WriteLine($"Fib{i} = {Fibonacci(i)}");
+}
---
+++ Example018_FunctionalC#/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string text = "(1,2) (3,4) (8,6) (7,8)"
+                .Replace("(", "")
+                .Replace(")", "");
+                
+Console.WriteLine(text);                
+var data = text.Split(" ")//возьми текст и разбей(Split) с учетом пробелов(" ")
+                .Select(item => item.Split(','))//дальше разбей текст с учетом символа-разделителя ','.
+                .Select(e =>(int.Parse(e[0]), int.Parse(e[1])))
+                              //Item1           //Item2
+                .Where(e => e.Item1 % 2 != 0)// сделает выборку координат, которые начинаются на четное число
+                .Select(point => (point.Item1*10, point.Item2*10))
+                .ToArray();
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    // for (int k = 0; k < data[i].Length; k++)
+    // {
+    //     Console.WriteLine(data[i][k]);
+    // }
+    Console.WriteLine();
+}
